77426bc1959a3a8cb3595d5b74aac18029969d9d
jonas.freiburg <jonas.freiburghaus@he-arc.ch>
2019-08-20 11:16:39 +0200

    add quiz page

--- server.js
+++ server.js
@@ -3,18 +3,28 @@ const app = express()
 const http = require('http').createServer(app)
 const path = require('path')
 const io = require('socket.io')(http)
+const Quiz = require('./personal_modules/Quiz')
 const viewPath = 'views'
 
-app.set('view engine', 'ejs');
+app.set('view engine', 'ejs')
+
+app.use(express.static('public'))
 
 app.get('/', (req, res) =>{
-  res.render('index');
+  res.render('index')
 })
 .get('/create_game', (req, res) =>{
   res.render('createGame', {gameId:'o9jd99'})
 })
-.get('/join_game/:id_party', (req, res) =>{
+.get('/join_game/:id_game', (req, res) => {
   console.log('join_game')
+})
+.get('/in_game/:id_game', (req, res) => {
+  let quiz = new Quiz('oqdb_breaking_bad.json')
+  quiz.readQuiz().then((quizData) => {
+    console.log(quizData.quizz.expert[0].question)
+    res.render('in_game', {quiz: quizData})
+  })
 })
 .use((req, res, next) => {
   console.log('404');
