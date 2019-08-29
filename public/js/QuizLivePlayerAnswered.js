73428cbf5cd71657826f4b80c94897e880cfec99
vincent.moulin1 <vincent.moulin@he-arc.ch>
2019-08-29 13:49:18 +0200

    added some features and modifications for playing until 15

--- public/js/QuizLivePlayerAnswered.js
+++ public/js/QuizLivePlayerAnswered.js
@@ -1,5 +1,5 @@
 class QuizLivePlayerAnswered {
-  constructor(socket){
+  constructor(socket) {
     this.socket = socket
     this.answeredPlayer = []
     this.divPlayerAnswered = document.getElementById('playerAnsweredContainer')
@@ -8,17 +8,21 @@ class QuizLivePlayerAnswered {
   /**
    * Display players who answered to the questionon]
    */
-  listenPlayerAnswered(){
+  listenPlayerAnswered() {
+
     this.socket.on('display_player_answered', (data) => {
 
       let newPseudo = data.arrayPlayer.filter(x => !this.answeredPlayer.includes(x))[0]//get the new connected player
 
-      this.answeredPlayer.push(newPseudo)//add new player to the local array
+      this.answeredPlayer.push(newPseudo) //add new player to the local array
+
+      if(newPseudo.length > 8) {
+        newPseudo = newPseudo.slice(0,5) + '...'
+      }
 
       let div = document.createElement('div')
-      div.innerHTML = '<img src="img/user.png" class="imgPlayerAnswered"/><h4 class="imgUserPseudo">'+ newPseudo +'</h4>'
-      div.classList.add('playerAnswered')
-
+      div.innerHTML = '<img src="img/user.png" class="imgPlayerAnsweredSmall"/><p class="imgUserPseudoSmall">'+ newPseudo +'</p>'
+      div.classList.add('playerAnsweredSmall')
       this.divPlayerAnswered.appendChild(div)
     })
   }
@@ -26,7 +30,7 @@ class QuizLivePlayerAnswered {
   /**
    * Clean the screen for a new question
    */
-  cleanScreen(){
+  cleanScreen() {
     this.answeredPlayer = []
     this.divPlayerAnswered.innerHTML = ''
   }
